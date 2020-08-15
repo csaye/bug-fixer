de3024700f986ac1869ba195382b4279b594e039
Cooper Saye <coopersaye@gmail.com>
2020-08-14 20:32:25 -0700

    Implement command entering.

---
+++ Assets/ProgramInput.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace BugFixer
+{
+    public class ProgramInput : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private TextMeshProUGUI pastInputText = null;
+
+        private TMP_InputField inputField;
+
+        private void Start()
+        {
+            inputField = GetComponent<TMP_InputField>();
+            inputField.ActivateInputField();
+        }
+
+        private void Update()
+        {
+            CheckSubmit();
+        }
+
+        private void CheckSubmit()
+        {
+            if (Input.GetKeyDown("return"))
+            {
+                pastInputText.text = $"{inputField.text}\n" + pastInputText.text;
+                inputField.text = "";
+                inputField.ActivateInputField();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BugFixer
+{
+    public class QuitButton : MonoBehaviour
+    {
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
